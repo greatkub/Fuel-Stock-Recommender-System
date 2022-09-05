b49ce5bbe98a03f5f6093d454be47ca25ff6e53d
Jirapat Suwanjunee <suwanjunee987@gmail.com>
2022-09-05 13:01:29 +0700

    Adjust Folder

--- Pages/Afternoon.js
+++ Pages/Afternoon.js
@@ -1,30 +1,15 @@
 const puppeteer = require('puppeteer')
 //const nodeCron = require("node-cron")
 const sch = require("node-schedule")
-//const ora = require("ora");
-//const chalk = require("chalk");
 const fs = require("fs/promises")
 const mongo = require("mongodb").MongoClient
 
-// Connect to MongoDB Database
-const url = "mongodb+srv://Oildb:seniorproject2@fsrs-cluster.0flfd.mongodb.net/test";let db, TL
-mongo.connect(
-    url,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    (err, client) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      db = client.db("TL")
-      TL = db.collection("TL")
 
-async function VeederRoot() {
+
+async function AfternoonTLS450() {
    try {
        const date = Date.now();
+       console.time()
        const browser = await puppeteer.launch({
         headless: true,
         slowMo: 50,
@@ -33,24 +18,19 @@ async function VeederRoot() {
         args: ['--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService', '--no-sandbox',
         '--disable-extensions','--use-gl=egl', '--disable-setuid-sandbox'],
         ignoreDefaultArgs: ["--disable-extensions"],
-        //extraPrefsFirefox: {
-          // Enable additional Firefox logging from its protocol implementation
-          // 'remote.log.level': 'Trace',
-        //},
-        //executablePath: 'C:/Program Files/Mozilla Firefox/firefox.exe',
-        FIREFOX_BIN: '/app/vendor/firefox/firefox'
-        //executablePath: '/app/vendor/firefox/firefox'
-        //Selenium:WebDriver:Firefox:Binary.path = "/app/vendor/firefox/firefox"
+        extraPrefsFirefox: {
+        },
        })
+       
        const page = await browser.newPage()
        await page.goto('https://essotheone.thaiddns.com:4433/#LogIn')
        await page.type('#gwt-debug-userNameTextBox', "seniorproject");
        await page.type('#gwt-debug-userPasswordTextBox', "vmsseniorproject2");
        await Promise.all([
         page.click("#gwt-debug-signInButton"),
-       // page.click("#gwt-debug-tankItem4 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > a:nth-child(2)"),
         page.waitForNavigation(),
     ]);
+
     await page.waitForSelector('#gwt-debug-tankItem1 > table:nth-child(1)');
         // Get data from Veeder-root web page      
         let data = await page.evaluate(() => {
@@ -65,6 +45,7 @@ async function VeederRoot() {
             // const time = dat.getTi
              const date = `${day}/${month}/${year}`
            items.forEach((item) => {   
+
                  const select = item.querySelectorAll("#_paramName.tank_item_div_height");
                  const TankName = item.querySelector('.TankLabel');
                  const T = TankName;
@@ -79,14 +60,9 @@ async function VeederRoot() {
                     Waterheight: W.innerText,
                     Fuelheight: F.innerText
                   })
-                   //url: item.getAttribute('data-url'),
-                   //Tank: item.querySelector('.TankLabel').innerText,
-                   //Volume: item.querySelector('.tank_item_div_height').textContent,
-                   //Ullage: item.querySelector('#gwt-debug-tankItem1 > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > div:nth-child(1) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1)').textContent
                });
              results.push({
                     date,
-                    //Time: D.innerText, 
                     Tank: allTank
                 });
                return results
@@ -98,8 +74,6 @@ async function VeederRoot() {
        console.error(error)
    }
 }
-sch.scheduleJob("0 8 * * *",VeederRoot);
-//sch.scheduleJob("*/30 * * * * *",VeederRoot);
-VeederRoot()
-//sch.cancelJob()
-})
+/* schedule allow to scrape data every 12:00 pm*/
+sch.scheduleJob("0 12 * * *",AfternoonTLS450);
+AfternoonTLS450()
